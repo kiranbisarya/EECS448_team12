fee28f2162d9a962c7b83919d8afbb35832d9412
Katie Lucas <katie.lucas.a@gmail.com>
2020-10-27 16:05:25 -0500

    Rough outline of some of the necessary functions

--- Game.js
+++ Game.js
@@ -1,4 +1,4 @@
-let player = 0;
+let currPlayer = 0;
 let turn = 0;
 let board = [[]];
 let cols = 6;
@@ -7,18 +7,48 @@ for(let i=0; i<rows; i++)
 {
     for(let j=0; j<cols; j++)
     {
-        board[i][j] = 0;
+        board[i][j]=0;
     }
 }
 
-if(gameOver())
+if(!gameOver(chosenRow, chosenCol))
+{
+    if(validChoice(chosenRow, chosenCol))
+    {
+        dropChip(chosenRow, chosenCol);
+        currPlayer++;
+    }
+    else
+    {
+        document.querySelector("#msg").innerText = "Invalid placement, try again";
+    }
+}
+
+dropChip(chosenRow, chosenCol)
+{
+    board[chosenRow][chosenCol] = 1;
+}
+
+validChoice(chosenRow, chosenCol)
+{
+    if(chosenRow>=0 && chosenRow<7 && chosenRow>=0 && chosenRow<6 && board[chosenRow][chosenCol]==0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+if(gameOver(chosenRow, chosenCol))
 {
 
 }
 else
 {
     turn++;
-    if(player%2==0)
+    if(currPlayer%2==0)
     {
         document.querySelector("#msg").innerText = "There is still no winner. Next up is player 1 for turn number " + turn;
     }
